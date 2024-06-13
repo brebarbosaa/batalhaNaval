2777c7aacd70c3a8ec51c4dfca0a2e2a0463a2ac
Brenda <151410709+brebarbosaa@users.noreply.github.com>
2024-06-13 17:56:06 -0300

    Update batalhaNaval.py

--- batalhaNaval.py
+++ batalhaNaval.py
@@ -7,9 +7,6 @@ def imprimir_tabuleiro(tabuleiro):
     for linha in tabuleiro:
         print(" ".join(linha))
 
-def criarTabuleiroEscondido():
-    return [['0' for _ in range(10)] for _ in range(10)]
-
 def coordenadasUsuario():
     while True:
         try:
@@ -19,6 +16,18 @@ def coordenadasUsuario():
                 return x, y
             else:
                 print("Coordenadas devem estar entre 0 e 9. Tente novamente.")
+        except ValueError:
+            print("Por favor, digite números inteiros. Tente novamente.")
+
+def ataqueJogador():
+    while True:
+        try:
+            ataqueX = int(input("Qual linha quer atacar? digite um número (0-9): "))
+            ataqueY = int(input("Qual coluna quer atacar? digite um número (0-9): "))
+            if 0 <= ataqueX < 10 and 0 <= ataqueY < 10:
+                return ataqueX, ataqueY
+            else:
+                print("Os valores devem estar entre 0 e 9, por favor tente novamente.")
         except ValueError:
             print("Por favor, digite números inteiros. Tente novamente.")
 
@@ -38,36 +47,51 @@ def tabuleiroAmostra():
 
 tabuleiroAmostra()
 
-print("embarcações restantes:", embarcacoes)
-
 # Função para criar tabuleiro 10x10 com embarcações em posições aleatórias
 def tabuleiroEscondido():
-    print("tabu")
     tabuleiro = [[0 for _ in range(10)] for _ in range(10)]
     for _ in range(10):
         linha = random.randint(0, 9)  # gera um número aleatório de 0 a 9 para a coordenada x
         coluna = random.randint(0, 9)  # gera um número aleatório de 0 a 9 para a coordenada y
         if tabuleiro[linha][coluna] == 0:
-            tabuleiro[linha][coluna] = 1
-    for linha in tabuleiro:
-            print(linha)
+            tabuleiro[linha][coluna] = 'X'
+            break
+    # for linha in tabuleiro:
+    #         print(linha)
 
-        
+
 def main():
     embarcacoes = 5
-    tabuleiro = criarTabuleiro()
+    tabuleiroJogador = criarTabuleiro()
+    tabuleiroComputador = criarTabuleiro()
 
-    print("Posicione suas embarcações no tabuleiro.")
+    print("Seja bem vindo ao Batalha Naval!")
 
     for _ in range(embarcacoes):
-        imprimir_tabuleiro(tabuleiro)
+        imprimir_tabuleiro(tabuleiroJogador) ## mudar pra tabuleiro escondido
         while True:
             x, y = coordenadasUsuario()
-            if embarcacoesPosicionadas(tabuleiro, x, y):
+            if embarcacoesPosicionadas(tabuleiroJogador, x, y):
                 break
 
-    print("Todas as embarcações foram posicionadas.")
-    imprimir_tabuleiro(tabuleiro)
+    tabuleiroEscondido()
 
+    ## mudar pra tabuleiro escondido
+    ## modo ataque jogador
+    print("agora é a vez de atacar o tabuleiro do computador, boa sorte!")
+
+    while True:
+        x, y = ataqueJogador()
+        if tabuleiroComputador[x][y] == 'X':
+            print("parabéns! você acertou uma embarcação do computador!")
+            ## mudar a coordenada do tabuleiro do computador para 'A' quando a embarcação for afundada
+            tabuleiroComputador[x][y] = 'A'
+            break
+        else:
+            print("você não acertou nenhuma embarcação do computador :( tente novamente!")
+
+## fazer uma variavel para poder printar no final quem ganhou
+## ex: se o computador ganhou = computadorGanhou
+    print("fim de jogo! ")
 if __name__ == "__main__":
     main()
